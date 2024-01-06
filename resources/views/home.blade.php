edddb8b5e28eb88833c4b8050deb1adef7e1528c
cluxnei <cluxnei31@gmail.com>
2024-01-05 22:04:11 -0300

    refactor: extract copyright to component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,10 +22,6 @@
             </div>
         </div>
 
-        <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
-            <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                Copyright, Random Stuff {{ now()->format('Y') }}, all rights reserved.
-            </div>
-        </div>
+        @include('layouts.copyright')
     </div>
 @endsection
